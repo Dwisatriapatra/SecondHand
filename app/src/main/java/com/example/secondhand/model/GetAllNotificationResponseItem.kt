5ef598418ee71541ee02f5eaf99f592679bf2922
Dwisatriapatra <dwisatriapatra2000@gmail.com>
2022-07-15 19:54:55 +0700

    update notification data model

--- app/src/main/java/com/example/secondhand/model/GetAllNotificationResponseItem.kt
+++ app/src/main/java/com/example/secondhand/model/GetAllNotificationResponseItem.kt
@@ -5,16 +5,22 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class GetAllNotificationResponseItem(
-    val bid_price: Int?,
-    val buyer_name: String?,
-    val created_at: String?,
-    val id: Int?,
-    val image_url: String?,
-    val product_id: Int?,
-    val read: Boolean?,
-    val receiver_id: Int?,
-    val seller_name: String?,
-    val status: String?,
-    val transaction_date: String?,
-    val updated_at: String?
+    val Product: ProductX,
+    val User: UserX,
+    val base_price: String,
+    val bid_price: Int,
+    val buyer_name: String,
+    val createdAt: String,
+    val id: Int,
+    val image_url: String,
+    val notification_type: String,
+    val order_id: Int,
+    val product_id: Int,
+    val product_name: String,
+    val read: Boolean,
+    val receiver_id: Int,
+    val seller_name: String,
+    val status: String,
+    val transaction_date: String,
+    val updatedAt: String
 ) : Parcelable
